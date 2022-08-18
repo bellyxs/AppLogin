45089bb79baf623c2ac15a3ef191f10013d2d4cf
bellyxs <bellarodgs@gmail.com>
2022-08-18 15:16:47 -0300

    first commit

---
+++ AppLogin2/AppLogin2/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+using AppLogin2.View;
+using AppLogin2.Model;
+
+namespace AppLogin2
+{
+    public partial class App : Application
+    {
+        private List<DadosUsuario> list_usuario = new List<DadosUsuario>
+        {
+            new DadosUsuario()
+            {
+                Email = "bella@gmail.com",
+                Nome = "Bella",
+                Senha = "202"
+            },
+            new DadosUsuario()
+            {
+                Email = "gatinhosfofos@gmail.com",
+                Nome = "Fã de gatos",
+                Senha = "456"
+            }
+        };
+
+        internal List<DadosUsuario> List_usuario { get => list_usuario; set => list_usuario = value; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (Properties.ContainsKey("usuario_logado"))
+                MainPage = new Protegida();
+            else
+                MainPage = new Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppLogin2/AppLogin2/Model/DadosUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppLogin2.Model
+{
+    internal class DadosUsuario
+    {
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
